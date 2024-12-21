1e9aa72d3e7cb4ec73fdcd0eb5d2d98fd384cf0f
Marie Chalard <marieamelie.1966@gmail.com>
2024-12-21 18:38:06 +0100

    fibo + gitignore executable

--- Module-01/ex04/fibonacci.ml
+++ Module-01/ex04/fibonacci.ml
@@ -1,11 +1,15 @@
+
+(* 
+  acc1 : Holds the Fibonacci number at position n -1
+  acc2: Holds the Fibonacci number at position n
+ *)
 let fibonacci n = 
   if n < 0 then -1
   else
-    let rec loop n = 
-      if n = 0 then 0
-      else if n = 1 then 1
-      else loop (n -2) + loop (n -1) 
-    in loop n
+    let rec loop n acc1 acc2 = 
+      if n = 0 then acc1
+      else loop (n - 1 ) acc2 ( acc1 + acc2)
+    in loop n 0 1
 
 
 let () =
